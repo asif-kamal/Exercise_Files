a1594f367beadb6ed7396fc2539eb64a1d521499
asif-kamal <asif.kamal1489@gmail.com>
2021-06-03 15:35:38 -0400

    Printing with indices of a string variable.

--- Ch02/02_06/strings.go
+++ Ch02/02_06/strings.go
@@ -13,4 +13,23 @@ func main() {
 
 	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
 	// uint8 is a byte
+
+	// book[0] = 116
+
+	fmt.Println(book[4:11])
+	fmt.Println(book[4:])
+	fmt.Println(book[:4])
+
+	fmt.Println("t" + book[1:])
+
+	fmt.Println("It was ½ price!")
+
+	poem := `
+	The road goes ever on
+	Down from the door where it began
+	...
+	`
+	fmt.Println(poem)
+
+
 }
